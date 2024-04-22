064a7917bd556343b30434ea338897ad6a8d7cd9
Zoe Neil <zneil@smith.edu>
2024-04-22 17:12:35 -0400

    campfire updates

--- Campfire.java
+++ Campfire.java
@@ -1,24 +1,54 @@
-// //import com.github.google.guava.;
-// //import com.google.common.*;
+import java.util.Scanner;
+import com.google.common.graph.*;
+import com.google.common.graph.Traverser;
 
-// public class Campfire {
+public class Campfire {
+    // //replenish health at campfire
+    // public void health_replenish(){
+    //     if (this.character.health != 0){
+    //         this.character.health += (rest + this.character.alliance);
+    //         }
+    //     }
     
-//     //replenish health at campfire
-//     public void health_replenish(){
-//         if (this.character.health != 0){
-//             this.character.health += (rest + this.character.alliance);
-//             }
-//         }
+    //learn how dialgoue trees work
+
+public static void main(String[] args) {
     
-//     //learn how dialgoue trees work
+    ImmutableNetwork<String, String> myGraph =
+        NetworkBuilder.directed()
+        .allowsParallelEdges(true)
+        .<String, String>immutable()
+        .addEdge("beginning", "option 1", "A")
+        .addEdge("beginning", "option 2", "B")
+        .addEdge("beginning", "option 3", "C")
+        .addEdge("beginning", "option 4", "D")
+        .build();  
 
-//     ImmutableGraph<Integer> graph =
-//     GraphBuilder.directed()
-//         .<Integer>immutable()
-//         .addNode(1)
-//         .putEdge(2, 3) // also adds nodes 2 and 3 if not already present
-//         .putEdge(2, 3) // no effect; Graph does not support parallel edges
-//         .build();
+    System.out.println(myGraph);
 
-//     Set<Integer> successorsOfTwo = graph.successors(2); // returns {3}
-// }
+    Traverser.forGraph(myGraph).breadthFirst("beginning")
+                .forEach(x->System.out.println(x));
+
+    Scanner sc = new Scanner(System.in);
+
+    String userInput = "A"; //sc.nextLine()
+    
+    String currentLocation = "beginning";
+
+    System.out.println("\n Current Location: " + currentLocation);
+
+    // while (currentLocation != myGraph.incidentNodes(userInput)){
+    //     System.out.println("That's not an avaliable option. Try again.");
+    // }
+
+    for (String successor : myGraph.successors(currentLocation)){
+        if (myGraph.hasEdgeConnecting(currentLocation, successor) &&
+            myGraph.edgeConnecting(currentLocation, successor).get().equals(userInput)){
+                currentLocation = successor;
+                System.out.println("Your new location is: " + currentLocation);
+                break;
+            }
+    }
+
+}
+}
