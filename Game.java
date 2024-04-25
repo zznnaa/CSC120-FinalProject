19db4dcdf1b2e041f35dd764353eaad6b44bcc66
Chioma Opara <copara@smith.edu>
2024-04-24 22:27:33 -0400

    update game

--- Game.java
+++ Game.java
@@ -4,13 +4,11 @@ import java.util.Random;
 public class Game {
     public Human enemy;
     public Human character;
-    public Scanner sc;
     public boolean gameEnd;
     
     public Game(){
         this.enemy = new Human("enemy", 10, 5, 0, true);
         this.character = new Human("human", 10, 5, 5, false);
-        this.sc = new Scanner(System.in);
         this.gameEnd = false;
     }
 
@@ -26,6 +24,39 @@ public class Game {
         return random.nextInt();
     }
 
+    // advance battle by one more attack
+    public void advanceBattle(boolean fight){
+        //if any character dies, you lose the battle
+        if (!this.character.isAlive()){
+            boolean battleEnd = true;
+            System.out.println("Sorry you lost the battle.");
+        }
+
+        // if the character is alive, check if enemy is dead
+        else{
+            // if the enemy is dead, the character has won the battle
+            if (!this.enemy.isAlive()){
+                boolean battleEnd = true;
+                System.out.println("Congrats! You have won the battle");
+            }
+
+            // else either return an attack or retreat
+            else{
+                boolean battleEnd = true;
+                // return an attack
+                if (fight){
+                    this.character.attack(this.enemy);
+                }
+
+                // retreat
+                else{
+                    System.out.println("The captain has ordered the troop to retreat");
+                    System.out.println(this.character);
+                }
+            }
+        }
+    }
+
     //game method
     public void gameMethod(){
     System.out.println("Your game has started.");
@@ -37,9 +68,18 @@ public class Game {
 
 public static void main(String[] args) {
     Game game = new Game();
-    game.gameMethod();
-    
+    while(game.gameEnd == false){
+        while(currentBattle.battleEnd == false){
+            game.enemy.attack(game.character);
+            game.advanceBattle(true);
+        }
+    }   
 }
 }
 
 // A game consists of three battles
+
+// TO-DO:
+// Fix the battle as a class vs a method problem
+// Once this is done, make sure that Training as an instance of battle works
+// Figure out how to generate random numbers in a more efficient way
