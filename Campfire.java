5f301690187657118a198e3e5c563c0498719658
Zoe Neil <zneil@smith.edu>
2024-04-24 21:39:32 -0400

    updates in campfire

--- Campfire.java
+++ Campfire.java
@@ -29,6 +29,37 @@ public class Campfire {
 
 public static void main(String[] args) {
     
+    //network builder using strings
+    ImmutableNetwork<String, String> myGraph =
+        NetworkBuilder.directed()
+        .allowsParallelEdges(true)
+        .<String, String>immutable()
+        .addEdge("beginning", "option 1", "A")
+        .addEdge("beginning", "option 2", "B")
+        .build();  
+
+        //print string graph
+        System.out.println(myGraph);
+
+        //traverse options in string graph
+        Traverser.forGraph(myGraph).breadthFirst("beginning")
+        .forEach(x->System.out.println(x));
+
+        String userInput = "A"; //sc.nextLine()
+
+        String currentLocation = "beginning";
+
+        System.out.println("\n Current Location: " + currentLocation);
+
+        //navigating to different locations in string graph
+        if (myGraph.incidentNodes(userInput) != null){
+            currentLocation = myGraph.incidentNodes(userInput).target();
+            System.out.println("Your new location is: " + currentLocation);
+        } else {
+            System.out.println("That's not a valid user input. Enter A or B");
+            //userInput something something
+        }
+
     //hashtable that holds all the character's responses
     Hashtable ht = new Hashtable<String, String>();
     ht.put("beginning", "Hello. My name is Farfelle. I'm a warrior from the Far Woods.");
@@ -48,48 +79,42 @@ public static void main(String[] args) {
         .addEdge(ht.get("beginning"), ht.get("option 1"), ht2.get("A"))
         .addEdge(ht.get("beginning"), ht.get("option 2"), ht2.get("B"))
         .build();
-    
-    //network builder using strings
-    ImmutableNetwork<String, String> myGraph =
-        NetworkBuilder.directed()
-        .allowsParallelEdges(true)
-        .<String, String>immutable()
-        .addEdge("beginning", "option 1", "A")
-        .addEdge("beginning", "option 2", "B")
-        .build();  
 
-    //print hashtable graph
-    System.out.println(myGraph2);
+        //print hashtable graph
+        System.out.println("\n" + myGraph2);
 
-    //traverse options in hashtable graph
-    Traverser.forGraph(myGraph2).breadthFirst(ht.get("beginning"))
-                .forEach(x->System.out.println(x));
+        //traverse options in hashtable graph
+        Traverser.forGraph(myGraph2).breadthFirst(ht.get("beginning"))
+        .forEach(x->System.out.println(x));
 
-    Scanner sc = new Scanner(System.in);
+        String userInput2 = "A"; //sc.nextLine()
 
-    String userInput = "A"; //sc.nextLine()
-    
-    Object currentLocation = ht.get("beginning");
+        Object currentLocation2 = ht.get("beginning");
 
-    System.out.println("\n Current Location: " + currentLocation);
+        System.out.println("\n Current Location: " + currentLocation2);
+        
+        //for loop, for each node connected to beginning
+            //if user input equals one of the nodes then make new variable
+            //else return error
+        //if there are incident nodes connected to new input, move within graph
 
-    //navigating to different locations in string graph
-    if (myGraph.incidentNodes(userInput) != null){
-        currentLocation = myGraph.incidentNodes(userInput).target();
-        System.out.println("Your new location is: " + currentLocation);
-    } else {
-        System.out.println("That's not a valid user input. Enter A or B");
-        //userInput something something
-    }
+        if (userInput2.charAt(0) == ht2.get("A").toString().charAt(0)){
+            Object replaceUserInputwithNewVariable = ht2.get("A");
+        }
 
-    //navigating to different locations in hashtable graph
-    if (myGraph2.incidentNodes(userInput) != null){
-        currentLocation = myGraph2.incidentNodes(userInput).target();
-        System.out.println("Your new location is: " + currentLocation);
-    } else {
-        System.out.println("That's not a valid user input. Enter A or B");
-        //userInput something something
-    }
+        //navigating to different locations in hashtable graph
+        if (myGraph2.incidentNodes(userInput2) != null){
+            currentLocation2 = myGraph2.incidentNodes(userInput2).target();
+            System.out.println("Your new location is: " + currentLocation2);
+        } else {
+            System.out.println("That's not a valid user input. Enter A or B");
+            //userInput something something
+        }
+
+        //is there a way to indicate a specific key, value pair from a hashtable as the node in a graph?
+        
+
+
 
 
         
